01c0e4e709daac366a521f30af2dc03d38079266
Npranata <nathannelson348@gmail.com>
2025-03-20 16:53:16 -0400

    embed spotify to website

--- SongsHandler.js
+++ SongsHandler.js
@@ -7,7 +7,6 @@ class SongsHandler{
     static CLIENT_SECRET = '01c615f0104e4ce585ed871ae37f4490';
     static REFRESH_TOKEN = 'AQA7cG_FjXyApdq1aYiMpnDfovnxzpogKg4S44xSkWioVS-AsKtGdj4IsjAwk4nVIeA0vesSlhmQJ1QSRNyrf1pNlbdA8lvDfxeTWgcQ1CsCjGJ_ZQhrnDV10C3yRTC3AIw';
 
-
     async getAccessToken(){
         const app = express();
         const PORT = 3002;
@@ -35,14 +34,26 @@ class SongsHandler{
         .then(data=>{
             return data.access_token;
         })
-        .catch((error)=> {
-            throw new Error("Failure!")
-        })
 
     }
 
     async getPlaylist(){
-
+        const playlistUrl  = 'https://api.spotify.com/v1/playlists/3cnkhyqinMpD5O6f6qh5l4?si=eOwrMAD9QAOzMID8wjluiQ/tracks';
+        const accessToken = await this.getAccessToken();
+        return fetch(playlistUrl, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Failure");
+            }
+            return response.json()
+        })
+        .then(data=>{
+            return data.track.items;
+        })
     }
 
 }
@@ -52,6 +63,13 @@ class SongsHandler{
     songHandler.getAccessToken()
         .then(token => {
             console.log("Access Token:", token);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    songHandler.getPlaylist()
+        .then(playlist => {
+            console.log("Playlist:", playlist);
         })
         .catch(error => {
             console.log(error);
