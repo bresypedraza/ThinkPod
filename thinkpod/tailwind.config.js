077af62db673627f54721a229b3bd3bf024d49e1
Npranata <nathannelson348@gmail.com>
2025-03-28 12:50:05 -0500

    changing some dependencies

--- thinkpod/tailwind.config.js
+++ thinkpod/tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     "./index.html",                 
     "./src/**/*.{js,jsx,ts,tsx}", 
   ],
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'gray-rgba': 'rgba(209, 213, 219,0.5)'
+      },
+    },
   },
   plugins: [],
 };
