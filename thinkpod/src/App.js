7237384f0eaf968793060399d6ebba279d35b2c1
Npranata <nathannelson348@gmail.com>
2025-03-29 13:58:50 -0500

    Make css changes and background changes

--- thinkpod/src/App.js
+++ thinkpod/src/App.js
@@ -28,29 +28,24 @@ function App() {
     },[]);
 
   return (
-    <div className="App h-screen w-screen bg-gray-100 flex flex-col items-center justify-center overflow-hidden h0">
-      <header className="App-header">
-        
-      </header>
+    <div className="App">
       
-      <body className="bg-gray-100">
+      <body className="screen bg-gray-100">
+      <header>
+        <button className="bg-white text-black p-2 rounded-lg">
+          Click me
+        </button>
+      </header>
         <Timer />
-        <div className="min-h-screen flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-lg">
-          <h1 className="text-3xl font-bold text-gray-800">Hello, World!</h1>
-          <p className="text-gray-600 mt-2">React and Tailwind is finally ready!</p>
-          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Click Me
-          </button>
-        </div>
+      <div className="min-h-screen flex items-center justify-center">
 
-        <div className="background_setting absolute w-2/3 h-2/3 bg-gray-rgba bg-opacity-50 font-bold overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] rounded-xl p-5">
-          <div> 
-            <span  className="text-3xl flex items-center justify-center m-4">Background Setting</span> 
-          </div>
-          
+      <div className="background_setting absolute w-2/3 h-2/3 bg-gray-rgba bg-opacity-50 font-bold overflow-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] rounded-xl p-5">
+        <div> 
+          <span  className="text-3xl flex items-center justify-center m-4">Background Setting</span> 
         </div>
-        </div>  
+        
+      </div>
+      </div>  
 
         {/* This is just to make sure that the flask and react are properly connected*/}
         <p>
