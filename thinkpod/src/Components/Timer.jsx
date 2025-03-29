ff3dbf076fee122d0998a2bac607c190ccc24953
marigwell <jwellmarigmen@gmail.com>
2025-03-29 12:12:23 -0500

    made Timer.jsx - incomplete

--- thinkpod/src/Components/Timer.jsx
+++ thinkpod/src/Components/Timer.jsx
@@ -1,17 +1,33 @@
 import React, {useState, useEffect, useRef} from 'react';
 
-function Timer(){
-    const [isRunning, setIsRunning] = useState(false);
-    const [elapsedTime, setElapsedTime] = useState(0);
-    const intervalIdRef = useRef(null);
-    const startTimeRef = useRef(0);
+const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - minutes * 60);
+
+    if (minutes <= 10) minutes = '0' + minutes;
+    if (seconds <= 10) seconds = '0' + seconds;
+    return minutes + ":" + seconds;
+}
+
+function Timer({seconds}){
+    const [countdown, setCountdown] = useState(seconds);
+    const timerId = useRef();
+
 
     useEffect(() => {
-
-    }, [isRunning]);
+        if(countdown <= 0) {
+            clearInterval(timerId.current)
+            alert("END")
+        }
+    })
 
     function start(){
-
+        useEffect(() => {
+        timerId.current = setInterval(() => {
+            setCountdown(prev => prev - 1)
+        }, 1000)
+        return () => clearInterval(timerId.current)
+        }, [])
     }
 
     function stop(){
@@ -21,17 +37,12 @@ function Timer(){
     function reset(){
 
     }
-    
-    function formatTime(){
-
-        return '00:00'
-    }
 
 
     return(
     <div className='Timer'>
-        <div className='bg-blue-400 rounded-lg w-auto h-auto place-self-end m-4'>
-            <div className='display font-bold text-center text-5xl p-10'>{formatTime()}</div>
+        <div className='bg-blue-500 rounded-lg w-auto h-auto place-self-end m-4'>
+            <div className='display font-bold text-center text-5xl p-10'>{formatTime(countdown)}</div>
             <div className='controls flex-col text-center font-sans'>
                 <button onClick={start} className='start-button bg-green-500 rounded m-2 w-[40px] h-[40px]'>Start</button>
                 <button onClick={reset} className='reset-button bg-gray-400 rounded m-2 w-[40px] h-[40px]'>Reset</button>
